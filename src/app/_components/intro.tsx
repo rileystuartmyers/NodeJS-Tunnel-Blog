79245dfc019a553fcd13b454252f88e933684bf0
rileystuartmyers <rileystuartmyers@gmail.com>
2025-10-21 15:27:31 -0500

    changed site bio

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -7,14 +7,7 @@ export function Intro() {
         stu4rt.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A personal blog and portfolio, statically generated using {" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+        My personal blog/portfolio. Hi!
       </h4>
     </section>
   );
